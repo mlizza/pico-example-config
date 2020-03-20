74fcb4eafec447c4471be6317e887368dec2d81f
Southclaws <hello@southcla.ws>
2020-03-20 13:06:16 +0000

    add quiet flag

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 T({
   name: "pico-test-one",
   url: "https://github.com/picostack/example-target",
-  up: ["docker-compose", "up", "-d"],
+  up: ["docker-compose", "up", "-d", "--quiet"],
   down: ["docker-compose", "down"]
 });
