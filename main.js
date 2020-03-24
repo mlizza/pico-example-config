725e49ee70228498255bfabed1ed6bd18e809e02
Southclaws <hello@southcla.ws>
2020-03-24 22:43:52 +0000

    add auth example

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ T({
   name: "pico-test-one",
   url: "https://github.com/picostack/example-target",
   up: ["docker-compose", "up", "-d", "--quiet"],
-  down: ["docker-compose", "down"]
+  down: ["docker-compose", "down"],
+  auth: "github"
+});
+
+A({
+  name: "github",
+  path: "auth_github",
+  user_key: "GIT_USERNAME",
+  pass_key: "GIT_PASSWORD"
 });
