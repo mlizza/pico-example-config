bc1a3268b76a63710cee5e140930062712c217ca
Massimo Lizza <52169261+mlizza@users.noreply.github.com>
2020-03-28 15:43:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 T({
   name: "test-web",
-  url: "https://github.com/mlizza/pico-example-target.git",
+  url: "https://github.com/mlizza/pico-example-target.git?ref=1.0.0",
   up: ["docker-compose", "up", "-d"],
   down: ["docker-compose", "down"]
 });
